81c80a222652c745c44fcce69031ad712e226ef4
Eric Wilhite <ericwilhite@users.noreply.github.com>
2022-04-04 14:44:05 -0400

    Delete dist directory

--- dist/tickets.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-  TOP 2
-  TicketNum AS TicketNumber,
-  Transactions_DateTimeTransaction AS TicketDateTime,
-  Customers_Customer_ID AS Customer_CustomerID
-FROM Transactions
-WHERE Transactions_DateTimeTransaction >= Date()-30
